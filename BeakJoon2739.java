f9561e9800b35954f9ee23e912d26dd647fb1233
Yu Knag <ykang9533@naver.com>
2020-09-02 00:20:43 +0900

    구구단 코드

---
+++ BeakJoon2739.java
@@ -0,0 +1,16 @@
+package java_test01;
+
+import java.util.Scanner;
+
+public class BeakJoon2739 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int a = 1; a < 10; a++) {
+			System.out.println(N + " * " + a + " = " + N * a);
+		}
+
+	}
+
+}
