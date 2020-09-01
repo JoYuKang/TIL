146940c5647214c18b47aa66cc5500a1fa3acbb8
Yu Knag <ykang9533@naver.com>
2020-09-02 00:34:37 +0900

    A+B - 3

---
+++ baekhjoon10950.java
@@ -0,0 +1,17 @@
+package java_test01;
+
+import java.util.Scanner;
+
+public class baekhjoon10950 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a+b);
+		
+		
+	}
+
+}
