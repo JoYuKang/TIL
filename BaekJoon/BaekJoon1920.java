8441a5ca9ab5b52964bb7af270d739fab5e1be31
Yu Knag <ykang9533@naver.com>
2020-10-21 00:15:18 +0900

    JAVA_CODE

---
+++ BaekJoon/BaekJoon1920.java
@@ -0,0 +1,49 @@
+package tomorrow;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BaekJoon1920 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringBuffer sb = new StringBuffer();
+
+		int num1 = Integer.parseInt(br.readLine());
+
+		int arr1[] = new int[num1];
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		for (int i = 0; i < num1; i++) {
+			arr1[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int num2 = Integer.parseInt(br.readLine());
+
+		int[] arr2 = new int[num2];
+
+		st = new StringTokenizer(br.readLine(), " ");
+
+		for (int i = 0; i < num2; i++) {
+			arr2[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr1);
+
+		for (int t = 0; t < arr2.length; t++) {
+			int index = Arrays.binarySearch(arr1, arr2[t]);
+			if (index < 0) {
+				sb.append(0 + "\n");
+			} else {
+				sb.append(1 + "\n");
+			}
+		}
+		System.out.println(sb);
+
+	}
+}
