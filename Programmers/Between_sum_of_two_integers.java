d1b200c7939a0bb55962bfb4d35c0590847dd975
JoYuKang <46198324+JoYuKang@users.noreply.github.com>
2020-12-21 14:50:23 +0900

    Update Between_sum_of_two_integers.java

--- Programmers/Between_sum_of_two_integers.java
+++ Programmers/Between_sum_of_two_integers.java
@@ -4,7 +4,7 @@ public class Between_sum_of_two_integers {
     public static void main(String[] args) {
         int a =3;
         int b =5;
-        System.out.println(solution(a,b));
+        System.out.println(solution(a,b)); 
     }
     static public long solution(int a, int b) {
         long answer = 0;
