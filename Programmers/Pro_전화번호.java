fe051ff0e87cb418fbdb429c8e06b91ba11d1f39
Yu Knag <ykang9533@naver.com>
2021-01-03 00:11:10 +0900

    JAVA_CODE

---
+++ Programmers/Pro_전화번호.java
@@ -0,0 +1,23 @@
+package todaytest;
+
+public class Pro_전화번호 {
+    public static void main(String[] args) {
+        String[] phone_book = {"119", "97674223", "1195524421"};
+        System.out.println(solution(phone_book));
+    }
+
+    static public boolean solution(String[] phone_book) {
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            for (int j = i + 1; j < phone_book.length; j++) {
+                //startsWith 대상 문자열이 특정 문자 또는 문자열로 시작하는지 체크하는 함수  boolean 에 맞춰 true/false 값을 리턴
+                if (phone_book[i].startsWith(phone_book[j])) {
+                    return false;
+                }
+                if (phone_book[j].startsWith(phone_book[i])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
