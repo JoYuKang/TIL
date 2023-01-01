aa98eaa95ff8a237c608b8f85e9e7f32d2a8872c
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2023-01-02 00:41:01 +0900

    dynamic

---
+++ inflearn/src/dynamic/돌다리건너기.java
@@ -0,0 +1,18 @@
+package dynamic;
+
+import java.util.Scanner;
+
+public class 돌다리건너기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[36];
+        arr[1] = 1;
+        arr[2] = 2;
+        arr[3] = 3;
+        for (int i = 3; i <= 35; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        System.out.println(arr[num + 1]);
+    }
+}
